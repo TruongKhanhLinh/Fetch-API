738dd44b34e94d870c7b0c69b5336b59c6e7798a
TruongKhanhLinh <39608219+TruongKhanhLinh@users.noreply.github.com>
2018-09-10 11:44:24 +0700

    Delete getExample.js

--- getExample.js
+++
@@ -1,8 +0,0 @@
-function getExample(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data) // Prints result from `response.json()` in getRequest
-    })
-    .catch(error => console.error(error))
-}
